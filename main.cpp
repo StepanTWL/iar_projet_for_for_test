313535771c43b0355d42264c1afc2b38a35b7e87
Stepan Dudar <dudar_sv@impulse.ua>
2025-09-05 14:24:53 +0300

    check googletest2

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "main.h"
+
+void test(int* x);
+void test1(int x);
+void test(int* x)
+{
+	test1(*x);
+}
+
+void test1(int x)
+{
+	x += 1;
+}
+
+int test2(int x) {
+    return x;
+}
+
+int main()
+{
+
+  int a = 0;
+
+  a = 2;
+
+  test(&a);
+  a = test2(a);
+
+  return 0;
+}
--- test_main.cpp
+++ test_main.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
+#include "main.h"
 
-extern "C" int test2(int x);
 
 TEST(Test2Suite, SimpleExample) {
     EXPECT_EQ(test2(1), 1); // замените на правильное ожидаемое значение
